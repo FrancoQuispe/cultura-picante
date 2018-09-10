7b510d62b884b2ea80dd0bff0e2d72f588a8e291
Mikel <francoquispe1995@gmail.com>
2018-09-09 23:11:43 -0500

    inicio-navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ViajesComponent } from './viajes/viajes.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { MusicaComponent } from './musica/musica.component';
+import { CreditosComponent } from './creditos/creditos.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MusicaComponent } from './musica/musica.component';
     NavbarComponent,
     ViajesComponent,
     InicioComponent,
-    MusicaComponent
+    MusicaComponent,
+    CreditosComponent
   ],
   imports: [
     AppRoutingModule,
